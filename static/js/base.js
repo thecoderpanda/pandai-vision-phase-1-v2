2f4c26aaef2e863b5530af022b54dd7a1707d785
Shantanu <mailvishwanadha@gmail.com>
2020-03-21 00:53:23 +0530

    New Dash

--- static/js/base.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by hussein on 5/17/17.
- */
-$(function () {
-    var id = $('title').html().split('|')[1].trim().replace(/ /g,'');
-    $('#'+id).addClass('active');
-});
--- tracker/apps.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from django.apps import AppConfig
-
-
-class TrackerConfig(AppConfig):
-    name = 'tracker'
-    verbose_name = 'Tracker application'
